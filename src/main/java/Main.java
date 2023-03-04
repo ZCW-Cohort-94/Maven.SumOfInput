0a643ea86cba7cde61c3b3ef70d42b13009a4f22
Jake Lawhorne <jake.lawhorne@gmail.com>
2023-03-04 00:37:59 -0500

    First function and gaussian function solved.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,33 @@
+import java.util.*;
 /**
+ *
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+
+        Main thing = new Main();
         
-        int number = getInputNumber();
+        int number = thing.getInputNumber();
         
-        int sum = sumOfNumbers(number);
+        int sum = thing.sumOfNumbers(number);
+
+        int gauss = thing.gaussianSumOfNumbers(number);
         
         System.out.println(sum);
+        System.out.println(gauss);
 
     }
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
     int sumOfNumbers(int n) {
-        return 0;
+        int sum = 0;
+        for(int i = 0; i <= n; i++){
+            sum += i;
+        }
+        return sum;
     }
     
     
@@ -36,9 +47,14 @@ public class Main {
     
     // for Extra Credit
     int gaussianSumOfNumbers(int n) {
-        return 0;
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum = (i / 2) * ( i + 1 );
+        }
+        return sum;
     }
-   
+
+
     // for Extra extra credit - compare the methods and show which one is faster
     // google "how to time some java code"
     // use "System.currentTimeMillis()" to ask the system what time it is.
